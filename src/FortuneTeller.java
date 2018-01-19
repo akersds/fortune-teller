ef2a0dc4be41024336bc936d93fcf288593e25b9
Deborah Akers <akersds18@gmail.com>
2018-01-19 13:13:50 -0500

    Added Part 2. Retirement Years

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -8,44 +8,34 @@ public class FortuneTeller {
 
 		System.out.println("Enter your first name");
 		String firstName = input.next().trim();
-		
+
 		System.out.println("Enter your last name");
 		String lastName = input.next().trim();
-		
+
 		System.out.println("Enter your favorite ROYGBIV color");
 		String favoriteROYGBIV = input.next();
-		
+
 		System.out.println("Enter help for list of your favorite ROYGBIV color");
 		String helpROYGBIV = input.next();
-		
+
 		System.out.println("Enter your number of siblings");
 		int numberSiblings = input.nextInt();
-		
+
 		System.out.println("Enter your age");
 		int ageNumber = input.nextInt();
-		
-		System.out.println("Enter your birth month");
-		int birthMonth = input.nextInt();
 
-		
-		
-		
-		
-		
-		
-		
-		
-		System.out.println(firstName + " " + lastName);
-
-		if (firstName.length() > lastName.length()) {
-			System.out.println(firstName + " is longer.");
-		} else if (lastName.length() > firstName.length()) {
-			System.out.println(lastName + " is longer.");
+		if (ageNumber % 2 == 0) { // if ageNumber is evenly divisible by 2
+			System.out.println(ageNumber + "is even");
+			String yearsRetire1 = input.nextLine();
+			System.out.println("You will retire in 12 years");
 		} else {
-			System.out.println("Both names are the same length.");
+			System.out.println(ageNumber + " is odd");
+			String yearsRetire2 = input.nextLine();
+			System.out.println("You will retire in 14 years");
+
+			System.out.println("Enter your birth month");
+			int birthMonth = input.nextInt();
+
 		}
-
-		input.close();
 	}
-
 }
